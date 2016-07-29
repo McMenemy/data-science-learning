ca57a4ba5990d8c13706903a89420c51d6f80b1e
McMenemy <mr_mcmenemy@yahoo.com>
2016-07-28 17:03:07 -0700

    submit compute cost

--- coursera-machine-learning/machine-learning-ex1/ex1/computeCost.m
+++ coursera-machine-learning/machine-learning-ex1/ex1/computeCost.m
@@ -12,10 +12,9 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
-
-
-
+m = length(X(:, 1));
+h = theta(1) * X(:, 1) + theta(2) * X(:, 2);
+J = sum((h - y).^2) / (2 * m); 
 
 % =========================================================================
 
