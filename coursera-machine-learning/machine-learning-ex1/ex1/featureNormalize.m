2c9c0ad950411135db99e84ad69f271266d3ef6d
McMenemy <mr_mcmenemy@yahoo.com>
2016-07-29 11:16:59 -0700

    complete feature normilization

--- coursera-machine-learning/machine-learning-ex1/ex1/featureNormalize.m
+++ coursera-machine-learning/machine-learning-ex1/ex1/featureNormalize.m
@@ -9,7 +9,6 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 X_norm = X;
 mu = zeros(1, size(X, 2));
 sigma = zeros(1, size(X, 2));
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: First, for each feature dimension, compute the mean
 %               of the feature and subtract it from the dataset,
@@ -25,14 +24,11 @@ sigma = zeros(1, size(X, 2));
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
-
-
-
-
-
-
-
-
+for i = 1:length(X(1, :))
+	mu(i) = mean(X(:, i));
+	sigma(i) = std(X(:, i));
+	X_norm(:, i) = (X(:, i) - mu(i)) / sigma(i);
+end
 
 % ============================================================
 
