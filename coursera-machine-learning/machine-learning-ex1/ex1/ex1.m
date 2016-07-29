0c7155dfed54209058f86323118accdfa8602e10
McMenemy <mr_mcmenemy@yahoo.com>
2016-07-29 09:26:23 -0700

    complete gradient descent

--- coursera-machine-learning/machine-learning-ex1/ex1/ex1.m
+++ coursera-machine-learning/machine-learning-ex1/ex1/ex1.m
@@ -61,7 +61,7 @@ alpha = 0.01;
 
 % compute and display initial cost
 computeCost(X, y, theta)
-pause;
+
 % run gradient descent
 theta = gradientDescent(X, y, theta, alpha, iterations);
 
--- coursera-machine-learning/machine-learning-ex1/ex1/gradientDescent.m
+++ coursera-machine-learning/machine-learning-ex1/ex1/gradientDescent.m
@@ -16,18 +16,14 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
+	% t0 - a/m sum ho - y
+	h = theta(1) * X(:, 1) + theta(2) * X(:, 2);
+	theta(1) = theta(1) - (alpha / m) * sum(h - y);
+	theta(2) = theta(2) - (alpha / m) * ((h - y)' * X(:, 2));
+	
     % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
-
 end
-
 end
