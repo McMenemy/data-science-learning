33f9e56595fb87d71f494208ece8c7d1cad31a8d
McMenemy <mr_mcmenemy@yahoo.com>
2016-07-29 22:49:01 -0700

    complete multivariable gradient descent

--- coursera-machine-learning/machine-learning-ex1/ex1/gradientDescentMulti.m
+++ coursera-machine-learning/machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,17 +16,10 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
-
+	h = X * theta;
+	for i = 1:length(X(1, :));
+		theta(i) = theta(i) - (alpha / m) * ((h - y)' * X(:, i));
+	end
     % ============================================================
 
     % Save the cost J in every iteration    
