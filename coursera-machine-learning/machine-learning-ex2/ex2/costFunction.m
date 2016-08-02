353e8b36159480275b42b4bdfe088c217a849737
McMenemy <mr_mcmenemy@yahoo.com>
2016-08-02 12:38:10 -0700

    complete logistic regression cost function and gradient

--- coursera-machine-learning/machine-learning-ex2/ex2/costFunction.m
+++ coursera-machine-learning/machine-learning-ex2/ex2/costFunction.m
@@ -19,13 +19,11 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+J = sum(-y' * log(sigmoid(X * theta)) - (1 - y)' * log(1 - sigmoid(X * theta))) / m;
 
-
-
-
-
-
-
+for i = 1:length(grad)
+	grad(i) = sum((sigmoid(X * theta) - y)' * X(:, i));
+end
 
 % =============================================================
 
